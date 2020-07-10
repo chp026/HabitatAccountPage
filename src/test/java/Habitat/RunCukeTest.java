10be4f12b5bc5d4c8aeeb60c74041d947ae259de
chp026 <chp026@hotmail.com>
2020-07-10 21:12:29 +0100

    Second

--- src/test/java/Habitat/RunCukeTest.java
+++ src/test/java/Habitat/RunCukeTest.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "C:\\Users\\Chirag\\IdeaProjects\\Habitat\\src\\test\\Resource\\MyAccount.feature",
-plugin = {"html:target/cucumber-html-report"})
+plugin = {"html:target/cucumber-html-report",
+"json:target/cucumber-json-report.json"})
 
 public class RunCukeTest {
 }
